e15b527ced280036c2841a4454f78839a65a7708
Jose Perez <cientifico141@gmail.com>
2022-06-23 20:14:20 +0100

    todo oddara

---
+++ js/main.js
@@ -0,0 +1,21 @@
+fetch('../json/galeria.json')
+.then(res => res.json())
+.then(imagen =>{
+      console.log()
+      const fragment = document.createDocumentFragment()
+      for(i in imagen){
+            const div = document.createElement('div')
+      
+            div.innerHTML =`
+            <div class="card text-center">
+            <img src="../img/galería/Hamburguesas/${imagen[i].imagen}.png" class="card-img-top" alt="...">
+            <div class="card-body">
+            <p class="card-text">${imagen[i].descripcion}</p>
+            </div>
+            </div>`
+            // console.log(div)
+            fragment.appendChild(div)
+      };
+      document.getElementById('sandwich').appendChild(fragment)
+})//end fectch
+    
